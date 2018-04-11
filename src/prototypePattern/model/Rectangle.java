a6892237c1a2a4a489a50110610bac565eaf4c4c
单昊冬 <534344228@qq.com>
2018-04-11 17:21:24 +0800

    原型模式

---
+++ src/prototypePattern/model/Rectangle.java
@@ -0,0 +1,15 @@
+package prototypePattern.model;
+
+public class Rectangle extends Shape {
+
+	public Rectangle() {
+		super();
+		type = "Rectangle";
+	}
+
+	@Override
+	void draw() {
+		System.out.println("Inside Rectangle::draw() method.");
+	}
+
+}
